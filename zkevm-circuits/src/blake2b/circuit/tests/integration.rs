edde7c6f4090f5248e0f384ab704bc49ba81e1b4
Aleksei Vambol <77882392+AlekseiVambol@users.noreply.github.com>
2023-06-22 16:30:36 +0300

    Add files via upload

--- zkevm-circuits/src/blake2b/circuit/tests/integration.rs
+++ zkevm-circuits/src/blake2b/circuit/tests/integration.rs
@@ -1,9 +1,11 @@
 use hex::{ decode_to_slice };
 use std::{ array, convert::TryInto };
 use halo2_proofs::{ halo2curves::bn256::Fr, dev::MockProver };
-use blake2b_circuit::{ CompressionCircuit, CompressionInput, compress };
+use blake2b_circuit::{ CompressionCircuit, CompressionInput, blake2b };
 
-// EIP-152 test vectors 4-7
+// EIP-152 test vectors 4-7 describing the BLAKE2b compression function inputs with the corresponding outputs. The vectors 1-3   
+// do not describe the correct inputs and are not representable in the format used in the tested library. The number of rounds  
+// for the vector 8 is 2^32 - 1, so the corresponding input cannot be processed by a circuit instance 
 const EIP152_VECTORS: [[&str; 2]; 4] = [
     ["0000000048c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001", 
      "08c9bcf367e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d282e6ad7f520e511f6c3e2b8c68059b9442be0454267ce079217e1319cde05b"],
@@ -15,9 +17,11 @@ const EIP152_VECTORS: [[&str; 2]; 4] = [
      "b63a380cb2897d521994a85234ee2c181b5f844d2c624c002677e9703449d2fba551b3a8333bcdf5f2f7e08993d53923de3d64fcc68c034e717b9293fed7a421"]
 ]; 
 
-pub fn hex_to_input(x: &str) -> CompressionInput { 
+// Creates the description of a BLAKE2b compression function input, which corresponds to the    
+// specified EIP-152 test vector, in accordance with the format used in the tested library
+pub fn hex_to_input(v: &str) -> CompressionInput { 
     let mut buffer = [0u8; 213];
-    decode_to_slice(x, &mut buffer).expect("Hex input must be correct!");
+    decode_to_slice(v, &mut buffer).expect("Hex input must be correct!");
     assert!(buffer[212] <= 1, "Incorrect final block indicator flag!");
 
     CompressionInput {
@@ -29,6 +33,8 @@ pub fn hex_to_input(x: &str) -> CompressionInput {
     }
 }
 
+// Tests the circuit instance, which has height 2^17 and processes the BLAKE2b 
+// compression function inputs corresponding to the EIP-152 test vectors 4-7
 #[test]
 fn circuit_check() {
     let k = 17;
@@ -38,26 +44,30 @@ fn circuit_check() {
     prover.assert_satisfied();
 }
 
+// Tests the implementation of the BLAKE2b compression function. The inputs   
+// with the corresponding outputs are described by the EIP-152 test vectors 4-7
 #[test]
 fn compression_check() {
     let mut expected = [0u8; 64];  
     for vector in &EIP152_VECTORS {
         let input = hex_to_input(vector[0]);
-        let result = compress(input.r, &input.h, &input.m, input.t, input.f);
+        let result = blake2b::compress(input.r, &input.h, &input.m, input.t, input.f);
         decode_to_slice(vector[1], &mut expected).expect("Hex expected value must be correct!");
         let expected: [u64; 8] =  array::from_fn(|i| u64::from_le_bytes(expected[8 * i..][..8].try_into().unwrap()));            
         assert_eq!(result, expected); 
     }
 }
 
+// Tests the implementation of the "optimums" function,  
+// which associated with the CompressionCircuit struct
 #[test]
 fn optimums_check() {
     let inputs = [
         CompressionInput { r: 200, h: [0; 8], m: [1; 16], t: 2, f: true },
-        CompressionInput { r: 9000, h: [1; 8], m: [3; 16], t: 1, f: true }, 
+        CompressionInput { r: 9000, h: [1; 8], m: [3; 16], t: 1, f: true },
         CompressionInput { r: 10000, h: [2; 8], m: [6; 16], t: 0, f: true }];
     let optimums = CompressionCircuit::<Fr, 8>::optimums(&inputs);
     assert_eq!(optimums[0..18], [None; 18]);
     assert_eq!(optimums[18..22], [Some(8), Some(24), Some(48), Some(88)]);
-    assert_eq!(optimums[22..32],  [Some(128); 10]);   
+    assert_eq!(optimums[22..32], [Some(128); 10]);
 }
