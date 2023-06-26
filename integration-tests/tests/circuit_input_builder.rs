448f010867ffe9aaa9cf5a6e825c1b529e8960f9
xiaodino <ruby@taiko.xyz>
2023-06-25 23:54:09 -0700

    Revert changes

--- integration-tests/tests/circuit_input_builder.rs
+++ integration-tests/tests/circuit_input_builder.rs
@@ -17,7 +17,7 @@ async fn test_circuit_input_builder_block(block_num: u64) {
         cli,
         CircuitsParams {
             max_rws: 16384,
-            max_txs: 10,
+            max_txs: 1,
             max_calldata: 4000,
             max_bytecode: 4000,
             max_copy_rows: 16384,
