ff42ef7139c03b4fe5e8adf5beef6bbaae28ece3
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 10:36:07 +0200

    WiP

--- .github/cloudRun/run.sh
+++ .github/cloudRun/run.sh
@@ -2,7 +2,7 @@
 GITHUB_RUN_ID=$1
 export GOROOT="/usr/local/go"
 export GOPATH="$HOME/go"
-export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
+export PATH="$GOROOT/bin:$GOPATH/bin:$PATH:$HOME/.cargo/bin/"
 
 # Get the latest temp directory in the home directory
 current_dir="$HOME"/CI_Prover_Benches/"$GITHUB_RUN_ID"
