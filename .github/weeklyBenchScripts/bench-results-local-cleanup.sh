9cfd602073df5c1947b3262db247ee4968379e8a
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 09:15:19 +0200

    WiP

--- .github/weeklyBenchScripts/bench-results-local-cleanup.sh
+++ .github/weeklyBenchScripts/bench-results-local-cleanup.sh
@@ -6,5 +6,6 @@ PROVER_INSTANCE=$(cat ~/prover_instance_weekly_"$GITHUB_RUN_ID")
 echo "Prover instance at cleanup: $PROVER_INSTANCE"
 tccli cvm TerminateInstances --InstanceIds "[\"$PROVER_INSTANCE\"]" --ReleasePrepaidDataDisk True
 echo "Exiting bench-results-local-cleanup"
+rm ~/prover_instance_weekly_"$GITHUB_RUN_ID"
 pkill ssh
 exit 0
