1b7dd953b6f02f9d2969413e1663ab977cd54975
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 07:58:17 +0200

    WiP

--- .github/weeklyBenchScripts/bench-results-local-cleanup.sh
+++ .github/weeklyBenchScripts/bench-results-local-cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Performing cleanup..."
-PROVER_INSTANCE=$(cat prover_instance)
+PROVER_INSTANCE=$(cat ~/prover_instance)
 echo "Prover instance: $PROVER_INSTANCE"
 tccli cvm TerminateInstances --InstanceIds "[\"$PROVER_INSTANCE\"]" --ReleasePrepaidDataDisk True
 echo "Exiting bench-results-local-cleanup"
--- .github/weeklyBenchScripts/github-action-trigger.sh
+++ .github/weeklyBenchScripts/github-action-trigger.sh
@@ -5,7 +5,7 @@ cleanup() {
   sshpass -p $BENCH_RESULTS_PASS ssh -t -t -o StrictHostKeyChecking=no ubuntu@43.130.90.57 <<EOF
   echo "Started executing bench-results-local-cleanup" >&2
   $(<bench-results-local-cleanup.sh)
-  echo "Finished executing bench-local-cleanup" >&2
+  echo "Finished executing bench-results-local-cleanup" >&2
   exit 0
 EOF
 }
