094006355ede5eb893447139c9d2cfa011c801e6
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 09:59:45 +0200

    WiP

--- .github/weeklyBenchScripts/github-action-setup.sh
+++ .github/weeklyBenchScripts/github-action-setup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+ensure_ssh_and_sshpass_installed() {
+  # Check if 'ssh' is installed
+  if ! command -v ssh &>/dev/null; then
+    echo "ssh is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y openssh-client
+  else
+    echo "ssh is already installed."
+  fi
+
+  # Check if 'sshpass' is installed
+  if ! command -v sshpass &>/dev/null; then
+    echo "sshpass is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y sshpass
+  else
+    echo "sshpass is already installed."
+  fi
+}
+
+ensure_ssh_and_sshpass_installed
 
 echo "Triggering setup"
 sshpass -p $BENCH_RESULTS_PASS ssh -t -t -o StrictHostKeyChecking=no ubuntu@43.130.90.57  "bash -s" -- "$GITHUB_RUN_ID" <bench-results-setup.sh
