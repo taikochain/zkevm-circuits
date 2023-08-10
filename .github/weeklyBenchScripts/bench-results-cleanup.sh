ed22ba2c00e47cde2c9149d41084b4930bc057e5
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 09:35:28 +0200

    WiP

--- .github/weeklyBenchScripts/bench-results-cleanup.sh
+++ .github/weeklyBenchScripts/bench-results-cleanup.sh
@@ -2,10 +2,9 @@
 GITHUB_RUN_ID=$1
 echo "Performing cleanup... $GITHUB_RUN_ID"
 sleep 60
-PROVER_INSTANCE=$(cat ~/prover_instance_weekly_"$GITHUB_RUN_ID")
+PROVER_INSTANCE=$(cat $HOME/CI_Github_Trigger/$GITHUB_RUN_ID/prover_instance)
 echo "Prover instance at cleanup: $PROVER_INSTANCE"
 tccli cvm TerminateInstances --InstanceIds "[\"$PROVER_INSTANCE\"]" --ReleasePrepaidDataDisk True
 echo "Exiting bench-results-local-cleanup"
-rm ~/prover_instance_weekly_"$GITHUB_RUN_ID"
 rm -rf "$HOME/CI_Github_Trigger/$GITHUB_RUN_ID"
 exit 0
--- .github/weeklyBenchScripts/bench-results-local-trigger.sh
+++ .github/weeklyBenchScripts/bench-results-local-trigger.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit 1
 
-GITHUB_RUN_ID=$1
-
-PROVER_INSTANCE=$(tccli cvm RunInstances --InstanceChargeType POSTPAID_BY_HOUR --InstanceChargePrepaid '{"Period":1,"RenewFlag":"DISABLE_NOTIFY_AND_MANUAL_RENEW"}' --Placement '{"Zone":"na-toronto-1"}' --InstanceType S3.16XLARGE256 --ImageId img-487zeit5 --SystemDisk '{"DiskType":"CLOUD_BSSD", "DiskSize":80}' --InternetAccessible '{"InternetChargeType":"TRAFFIC_POSTPAID_BY_HOUR","InternetMaxBandwidthOut":10,"PublicIpAssigned":true}' --InstanceCount 1 --InstanceName BENCH-PROVER --LoginSettings '{"KeyIds":[ "skey-au79yarf" ]}' --SecurityGroupIds '["sg-c3jtjz5g"]' --HostName BENCH-PROVER | egrep -o ins-[0-9a-zA-Z]*)
-#PROVER_INSTANCE=$(tccli cvm RunInstances --InstanceChargeType POSTPAID_BY_HOUR --InstanceChargePrepaid '{"Period":1,"RenewFlag":"DISABLE_NOTIFY_AND_MANUAL_RENEW"}' --Placement '{"Zone":"eu-frankfurt"}' --InstanceType S5.16XLARGE256 --ImageId img-487zeit5 --SystemDisk '{"DiskType":"CLOUD_BSSD", "DiskSize":80}' --InternetAccessible '{"InternetChargeType":"TRAFFIC_POSTPAID_BY_HOUR","InternetMaxBandwidthOut":10,"PublicIpAssigned":true}' --InstanceCount 1 --InstanceName BENCH-PROVER --LoginSettings '{"KeyIds":[ "skey-au79yarf" ]}' --SecurityGroupIds '["sg-ajrlphkl"]' --HostName BENCH-PROVER | egrep -o ins-[0-9a-zA-Z]*)
-#PROVER_INSTANCE=$(tccli cvm RunInstances --InstanceChargeType POSTPAID_BY_HOUR --InstanceChargePrepaid '{"Period":1,"RenewFlag":"DISABLE_NOTIFY_AND_MANUAL_RENEW"}' --Placement '{"Zone":"na-ashburn-2"}' --InstanceType S3.MEDIUM2 --ImageId img-487zeit5 --SystemDisk '{"DiskType":"CLOUD_BSSD", "DiskSize":50}' --InternetAccessible '{"InternetChargeType":"TRAFFIC_POSTPAID_BY_HOUR","InternetMaxBandwidthOut":10,"PublicIpAssigned":true}' --InstanceCount 1 --InstanceName BENCH-PROVER --LoginSettings '{"KeyIds":[ "skey-au79yarf" ]}' --SecurityGroupIds '["sg-ajrlphkl"]' --HostName BENCH-PROVER | egrep -o ins-[0-9a-zA-Z]*)
-echo "$PROVER_INSTANCE" > ~/prover_instance_weekly_"$GITHUB_RUN_ID"
-echo "Prover instance at trigger: "
-cat ~/prover_instance_weekly_"$GITHUB_RUN_ID"
-sleep 60
-
 export PROVER_IP=$(tccli cvm DescribeInstances --InstanceIds "[\"$PROVER_INSTANCE\"]" | grep -A 1 PublicIpAddress | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 echo "Prover IP: $PROVER_IP"
 
