ffa84b9f667f984772535a89f690c14f9e62deda
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 10:38:44 +0200

    WiP

--- .github/cloudRun/github-action-trigger.sh
+++ .github/cloudRun/github-action-trigger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Triggering behchmark trigger"
+echo "Triggering Cloud Run trigger"
 sshpass -p $BENCH_RESULTS_PASS ssh -t -t -o StrictHostKeyChecking=no ubuntu@43.130.90.57 "bash -s" -- "$GITHUB_RUN_ID" <cloud-tests-trigger.sh
 
 echo "Exiting github-action-trigger"
