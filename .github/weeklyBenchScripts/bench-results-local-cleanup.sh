f23f5ce121da0dedfed66b817e31d83eeb34b20e
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 08:26:29 +0200

    WiP

--- .github/weeklyBenchScripts/bench-results-local-cleanup.sh
+++ .github/weeklyBenchScripts/bench-results-local-cleanup.sh
@@ -2,8 +2,8 @@
 echo "Performing cleanup..."
 echo "Prover instance: "
 cat ~/prover_instance_weekly
-PROVER_INSTANCE=$(cat ~/prover_instance_weekly)
-echo "Prover instance: $PROVER_INSTANCE"
+PROVER_INSTANCE=$(cat \~/prover_instance_weekly)
+echo "Prover instance at cleanup: $PROVER_INSTANCE"
 tccli cvm TerminateInstances --InstanceIds "[\"$PROVER_INSTANCE\"]" --ReleasePrepaidDataDisk True
 echo "Exiting bench-results-local-cleanup"
 pkill ssh
--- .github/weeklyBenchScripts/bench-results-local-trigger.sh
+++ .github/weeklyBenchScripts/bench-results-local-trigger.sh
@@ -5,8 +5,8 @@ PROVER_INSTANCE=$(tccli cvm RunInstances --InstanceChargeType POSTPAID_BY_HOUR -
 #PROVER_INSTANCE=$(tccli cvm RunInstances --InstanceChargeType POSTPAID_BY_HOUR --InstanceChargePrepaid '{"Period":1,"RenewFlag":"DISABLE_NOTIFY_AND_MANUAL_RENEW"}' --Placement '{"Zone":"eu-frankfurt"}' --InstanceType S5.16XLARGE256 --ImageId img-487zeit5 --SystemDisk '{"DiskType":"CLOUD_BSSD", "DiskSize":80}' --InternetAccessible '{"InternetChargeType":"TRAFFIC_POSTPAID_BY_HOUR","InternetMaxBandwidthOut":10,"PublicIpAssigned":true}' --InstanceCount 1 --InstanceName BENCH-PROVER --LoginSettings '{"KeyIds":[ "skey-au79yarf" ]}' --SecurityGroupIds '["sg-ajrlphkl"]' --HostName BENCH-PROVER | egrep -o ins-[0-9a-zA-Z]*)
 #PROVER_INSTANCE=$(tccli cvm RunInstances --InstanceChargeType POSTPAID_BY_HOUR --InstanceChargePrepaid '{"Period":1,"RenewFlag":"DISABLE_NOTIFY_AND_MANUAL_RENEW"}' --Placement '{"Zone":"na-ashburn-2"}' --InstanceType S3.MEDIUM2 --ImageId img-487zeit5 --SystemDisk '{"DiskType":"CLOUD_BSSD", "DiskSize":50}' --InternetAccessible '{"InternetChargeType":"TRAFFIC_POSTPAID_BY_HOUR","InternetMaxBandwidthOut":10,"PublicIpAssigned":true}' --InstanceCount 1 --InstanceName BENCH-PROVER --LoginSettings '{"KeyIds":[ "skey-au79yarf" ]}' --SecurityGroupIds '["sg-ajrlphkl"]' --HostName BENCH-PROVER | egrep -o ins-[0-9a-zA-Z]*)
 echo "$PROVER_INSTANCE" > ~/prover_instance_weekly
-echo "Prover Instance: "
-cat ~/prover_instance_weekly
+echo "Prover snstance at trigger: "
+cat \~/prover_instance_weekly
 sleep 60
 
 export PROVER_IP=$(tccli cvm DescribeInstances --InstanceIds "[\"$PROVER_INSTANCE\"]" | grep -A 1 PublicIpAddress | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
