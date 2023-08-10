45a79c3eaf54567d1e7decd3f196112e88948589
krzysztofpaliga <paligakrzysztof106@gmail.com>
2023-08-10 09:38:58 +0200

    WiP

--- .github/weeklyBenchScripts/bench-results-trigger.sh
+++ .github/weeklyBenchScripts/bench-results-trigger.sh
@@ -11,21 +11,6 @@ ensure_git_installed() {
   fi
 }
 
-make_temp_dir() {
-  # Set up the directory name with the timestamp
-  directory_name="CI_Github_Trigger/$GITHUB_RUN_ID"
-
-  # Set up the full path for the directory in the home directory
-  directory_path="$HOME/$directory_name"
-
-  # Create the directory
-  mkdir -p "$directory_path"
-
-  cd "$directory_path" || exit 1
-
-  echo "Directory '$directory_name' with GITHUB_RUN_ID '$GITHUB_RUN_ID' has been created in the home directory."
-}
-
 clone_zkevm-circuits() {
   git clone https://github.com/krzysztofpaliga/zkevm-circuits.git
   cd zkevm-circuits || exit 1
@@ -34,7 +19,6 @@ clone_zkevm-circuits() {
 }
 
 ensure_git_installed
-make_temp_dir
 clone_zkevm-circuits
 
 cd .github/weeklyBenchScripts || exit 1
