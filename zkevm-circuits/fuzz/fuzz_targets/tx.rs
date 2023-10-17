52e4cfcc34126ae6edda1e616cdd65161e013f1a
krzysztofpaliga <paligakrzychu@gmail.com>
2023-10-17 10:57:18 +0200

    bug fix

--- zkevm-circuits/fuzz/fuzz_targets/tx.rs
+++ zkevm-circuits/fuzz/fuzz_targets/tx.rs
@@ -21,13 +21,14 @@ use rand_chacha::ChaCha20Rng;
 
 
 
-const NTX: usize = 1;
+const CURRENT_NTX: usize = 1;
+const CURRENT_NACC: usize = 0;
 
 fuzz_target!(|tx_random_input: TxRandomInput| {
 
     println!("tx random input count: {:?}", tx_random_input.transactions_random_input.len());
 
-    if tx_random_input.transactions_random_input.len() != NTX {
+    if tx_random_input.transactions_random_input.len() != CURRENT_NTX {
         return;
     }
     if tx_random_input.transactions_random_to == [0; 20] {
@@ -37,8 +38,8 @@ fuzz_target!(|tx_random_input: TxRandomInput| {
     const MAX_CALLDATA: usize = 32;
     let chain_id: u64 = mock::MOCK_CHAIN_ID.as_u64();
 
-    let mut transactions = vec![MockTransaction::default(); NTX];
-    transactions = TransactionMember::<NTX>::randomize_transactions_vec_one_random_member(
+    let mut transactions = vec![MockTransaction::default(); CURRENT_NTX];
+    transactions = TransactionMember::<CURRENT_NTX>::randomize_transactions_vec_one_random_member(
         transactions,
         tx_random_input
     );
@@ -60,5 +61,5 @@ fuzz_target!(|tx_random_input: TxRandomInput| {
     }
 
     println!("Input: {:?}", transactions);
-    assert_eq!(run::<Fr>(transactions, chain_id, NTX, MAX_CALLDATA), Ok(()));
+    assert_eq!(run::<Fr>(transactions, chain_id, CURRENT_NTX, MAX_CALLDATA), Ok(()));
 });
