a1b7bcffe92f7d888ca8644d04a43ff3a8947893
krzysztofpaliga <paligakrzychu@gmail.com>
2023-10-25 10:39:22 +0200

    Adds Graphana dashboard for integration tests

--- .github/integrationBenchScripts/08_processResults.sh
+++ .github/integrationBenchScripts/08_processResults.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #set -eo pipefail
 
-label=$1
+prover=$1
 degree=$2
 
 # Get the latest temp directory in the Triggerers directory
-trigger_results_dir="../../../results/$label"
+trigger_results_dir="../../../results"
 mkdir -p "$trigger_results_dir" || true
 
 # Get the latest temp directory in the Provers home directory
@@ -43,7 +43,7 @@ scp -i ~/.ssh/bench.pem -o StrictHostKeyChecking=no ubuntu@"$PROVER_IP":"$prover
 
 # Prepare for and run data processing and db persistence
 
-l=$(echo "$label" | tr -d '"')
+l=$(echo "$prover" | tr -d '"')
 circuit=$(echo "$l" |  awk '{print $1}')
 time=$(date +%Y-%m-%d_%H-%M-%S)
 test_id=$time-$circuit-$degree-Benchmark
@@ -51,12 +51,46 @@ test_id=$time-$circuit-$degree-Benchmark
 cd "$trigger_results_dir"
 tar -czvf ./"$test_id".tar.gz ./*proverlog ./*.stats
 
-cp ../../zkevm-circuits/.github/weeklyBenchScripts/reporting*.py .
+cp ../zkevm-circuits/.github/integrationBenchScripts/reporting*.py .
 sudo cp *proverlog /var/www/www_logs/
 proverlog="http://43.130.90.57/www_logs/"$(ls -t /var/www/www_logs | head -1)
+instance_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*instance.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+instance_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*instance.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+advice_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*advice.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+advice_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*advice.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+fixed_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*fixed.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+fixed_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*fixed.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+lookups_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*lookups.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+lookups_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*lookups.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+equality_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*equality.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+equality_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*equality.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+vanishing_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*vanishing.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+vanishing_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*vanishing.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+multiopen_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*multiopen.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+multiopen_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*multiopen.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+polycomm_commitments=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*polycomm.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $1}')
+polycomm_evaluations=$(ls -t *proverlog | xargs cat | sed 's/\x1B\[[0-9;]*[JKmsu]//g' | egrep -o  "|.*polycomm.*" | egrep -o [0-9]+[\ ]+[0-9]+ | awk '{print $2}')
+
+circuit_cost=$(ls -t *proverlog | xargs cat | egrep -o "CircuitCost.*")
+
+minimum_rows=$(ls -t *proverlog | xargs cat | egrep -o "^minimum_rows.*" | egrep -o "[0-9]+")
+blinding_factors=$(ls -t *proverlog | xargs cat | egrep -o "^blinding_factors.*" | egrep -o "[0-9]+")
+gates_count=$(ls -t *proverlog | xargs cat | egrep -o "^gates count.*" | egrep -o "[0-9]+")
+
+
 sed -i '1i BENCH-PROVER;-1;UTC;LINUX-RESTART	(64 CPU)' mem.stats
 sed -i '1i BENCH-PROVER;-1;UTC;LINUX-RESTART	(64 CPU)' cpu.stats
-python3 reporting_main.py  "$proverlog" "1" "$circuit" "$degree" "$test_id" "metricA" "metricB"
+python3 reporting_main.py  "$proverlog" "1" "$prover" "$degree" "$test_id" \
+"$instance_commitments" "$instance_evaluations" \
+"$advice_commitments" "$advice_evaluations" \
+"$fixed_commitments" "$fixed_evaluations" \
+"$lookups_commitments" "$lookups_evaluations" \
+"$equality_commitments" "$equality_evaluations" \
+"$vanishing_commitments" "$vanishing_evaluations" \
+"$multiopen_commitments" "$multiopen_evaluations" \
+"$polycomm_commitments" "$polycomm_evaluations" \
+"$circuit_cost" \
+"$minimum_rows" "$blinding_factors" "$gates_count"
 
 ssh -i ~/.ssh/bench.pem -o StrictHostKeyChecking=no ubuntu@"$PROVER_IP" "bash -s" <<EOF
 sudo rm -rf $prover_results_dir
